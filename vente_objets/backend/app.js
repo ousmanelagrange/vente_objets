bfd8286fa2ca8e9da9a5348b40d894c2d52baa4b
ousmanelagrange <ousmanemfochive4@gmail.com>
2023-10-15 17:01:31 +0100

    Création d'une boutique test

--- vente_objets/backend/app.js
+++ vente_objets/backend/app.js
@@ -44,5 +44,28 @@ app.get('/api/stuff', (req, res, next) => {
 });
 
 
+app.get('/api/stuff/:id', (req, res, next) => {
+    Thing.findOne({_id : req.params.id})
+        .then(thing => {
+            if (!thing) {
+                return res.status(404).json({ message: "Objet non trouvé" });
+            }
+            res.status(200).json( thing );
+        })
+        .catch(error => res.status(500).json({ error }));
+});
+
+app.put('/api/stuff/:id', (req, res, next) => {
+    Thing.updateOne({_id : req.params.id}, {...req.body, _id : req.params.id})
+    .then(() => res.status(200).json({message : 'Objet modifié !'}))
+    .catch(error => res.status(400).json({error}));
+})
+
+app.delete('/api/stuff/:id', (req, res, next) => {
+    Thing.deleteOne({_id : req.params.id})
+    .then(() => res.status(200).json({message : 'Objet supprimé avec succès !!!'}) )
+    .catch(error => res.status(400).json({error}));
+})
+
 module.exports = app;
 
